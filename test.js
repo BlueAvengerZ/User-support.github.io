a808df16e2cc867f4001d232ee7d5a4fa7ae6f14
BlueAvengerZ <89189237+BlueAvengerZ@users.noreply.github.com>
2022-05-25 16:48:14 +0200

    Update test.js

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
               $('.menu-bar').removeClass("sticky");
             }
       });
-
+    });
      // sticky menu-bar on scroll script End
 
 // array med spørmålene og aternativene:
